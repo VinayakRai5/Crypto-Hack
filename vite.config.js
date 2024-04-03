bce28702a84c06c4ac8fdf056beaf86f0ad4323e
NVJKKartik <114015823+NVJKKartik@users.noreply.github.com>
2024-04-03 16:09:39 +0530

    Added Chatbot, first cycle, need improvements in UI

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import commonjs from 'vite-plugin-commonjs';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [commonjs({
+  plugins: [nodePolyfills(),commonjs({
     filter(id) {
       if (id.includes('node_modules/redux-storage/build-es')) {
         return true;
